526c0e2e8e60346976f46f5fc7145f884a987e4b
Máté Tamási <kresho17@gmail.com>
2025-03-05 18:32:56 +0100

    Change Router config

--- ticket_booking_system/api/urls.py
+++ ticket_booking_system/api/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path, include
+""" from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet, OrderViewSet, EventViewSet
+from .views import TicketViewSet, OrderViewSet, EventList
 
 router = DefaultRouter()
 router.register(r'tickets', TicketViewSet)
-router.register(r'events', EventViewSet)
+router.register(r'events', EventList)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+ """
+
+from django.urls import path
+from .views import EventList
+
+urlpatterns = [
+    path('events/', EventList.as_view(), name='event-list'),
 ]
