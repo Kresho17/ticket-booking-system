241ff0fe79222101ea90bc00548d92df1e182b8d
Máté Tamási <kresho17@gmail.com>
2025-03-05 18:32:24 +0100

    Add JWT auth settings and req

--- ticket_booking_system/ticket_booking_system/settings.py
+++ ticket_booking_system/ticket_booking_system/settings.py
@@ -132,3 +132,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'api.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
