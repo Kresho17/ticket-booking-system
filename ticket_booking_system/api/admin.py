b1b425e6eb76c1db9f4c0293290f13120dc7030d
Máté Tamási <kresho17@gmail.com>
2025-03-07 15:36:15 +0100

    Add ticket to admin site

--- ticket_booking_system/api/admin.py
+++ ticket_booking_system/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Event, Order
+from .models import User, Event, Order, Ticket
 
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Event)
 admin.site.register(Order)
+admin.site.register(Ticket)
